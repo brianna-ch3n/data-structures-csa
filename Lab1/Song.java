c08659e967eaf84aadc037213cb56033245be716
brianna-ch3n <brianna666888@gmail.com>
2026-05-28 22:24:48 -0700

    Update Song.java

--- Lab1/Song.java
+++ Lab1/Song.java
@@ -1,5 +1,28 @@
-package Lab1; //can delete
-
 public class Song {
-    
+   private String title;
+   private String artist;
+   private String album;
+ 
+   public Song(String title, String artist, String album)
+   {
+      this.title  = title;
+      this.artist = artist;
+      this.album  = album;
+   }
+ 
+   public String getTitle(){
+       return title;
+   }
+   public String getArtist(){
+       return artist;
+   }
+   public String getAlbum(){
+       return album;
+   }
+ 
+   public String toString()
+   {
+      return "\"" + title + "\" by " + artist + " [" + album + "]";
+   }
 }
+ 
