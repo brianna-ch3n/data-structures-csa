4bb81426d003a48f4b7c95ed1e53ba9038a78556
brianna-ch3n <brianna666888@gmail.com>
2026-05-30 13:31:58 -0700

    Update Playlist.java

--- Lab2/Playlist.java
+++ Lab2/Playlist.java
@@ -1,3 +1,37 @@
-public class Playlist{
+import java.util.LinkedList;
 
+public class Playlist{
+  private LinkedList<Song> songs = new LinkedList<>();
+}
+
+public void addToFront(Song song) {
+    songs.addFirst(song);
+}
+
+public void addToEnd(Song song){
+    songs.addLast(song);
+}
+
+public Song nowPlaying(){
+    return songs.peekFirst();
+}
+
+public Song skip(){
+    if (songs.isEmpty())
+    {
+        return null;
+    }
+
+    return songs.removeFirst();
+}
+
+public int size(){
+    return songs.size();
+}
+
+public void printPlaylist(){
+    for (Song s : songs)
+    {
+        System.out.println(s);
+    }
 }
